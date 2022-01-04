a65f4b47ca74f0e141d196f11dd0d00020716832
shkirya.ns <shkirya.ns@oilpc.ru>
2022-01-04 21:17:55 +0600

    attempts translate per day

--- core/__init__.py
+++ core/__init__.py
@@ -1,5 +1,6 @@
 from time import sleep
 import os
+import asyncio
 
 from .logger import logger
 from core import configurations
@@ -22,7 +23,7 @@ class VoicerConfig:
             self.tts_enable = tts_enable
 
 
-class Voicer:
+class VoiceApplication:
     tts_enable = True
     config: VoicerConfig = None
 
@@ -40,88 +41,107 @@ class Voicer:
             except ValueError:
                 pass
 
-    def handler(self, message: TelegramMessage):
+    async def handler(self, message: TelegramMessage):
         '''
         Обработчик входящего сообщения
         :param message:
         :return:
         '''
         # Обрабатываем текстовое сообщение
-        user = User(message.chat_id)
-        ya_app = yandex.Yandex()
-        if message.text:
+        try:
+            user = User(message.chat_id)
+            ya_app = yandex.Yandex()
+
             # Команда
-            if message.text.startswith('/'):
+            if message.text and message.text.startswith('/'):
                 if user.user_id:
-                    user.command_processing(message)
+                    command, value = user.command_processing(message)
+                    bot.send_message(message.chat_id, f'Параметр {command} изменен на {value}')
                 else:
                     self.command_processing(message)
+
+            # Переводим текст
             else:
-                # Переводим текст
-
-                ya_app.translate(message.text, settings=user)
-                # Возвращаем голосовое сообщение
-                # если разрешено
-                if self.config.tts_enable and user.tts:
-                    voice = ya_app.tts(ya_app.text, settings=user).file
-                    if os.path.exists(voice):
-                        with open(voice, 'rb') as f:
-                            data_file = f.read()
-                        bot.send_voice(message.chat_id, voice=data_file, caption=ya_app.text)
-                        os.remove(voice)
-                # текстовое сообщение
+                # Проверяем кол-во доступных попыток
+                if not user.attempts_per_day:
+                    bot.send_message(message.chat_id, 'Лимит исчерпан')
                 else:
-                    bot.send_message(message.chat_id, ya_app.text)
+                    if message.text:
+                        ya_app.translate(message.text, settings=user)
+                        # Возвращаем голосовое сообщение
+                        # если разрешено
+                        if self.config.tts_enable and user.tts:
+                            voice = ya_app.tts(ya_app.text, settings=user).file
+                            if os.path.exists(voice):
+                                with open(voice, 'rb') as f:
+                                    data_file = f.read()
+                                bot.send_voice(message.chat_id, voice=data_file, caption=ya_app.text)
+                                os.remove(voice)
+                        # текстовое сообщение
+                        else:
+                            bot.send_message(message.chat_id, ya_app.text)
+                        # Уменьшаем остаток проб перевода
+                        user.attempt_decrement()
 
-        # Обрабатываем голосовое сообщение
-        if message.voice:
-            file_id = message.voice.get('file_id')
-            file = bot.get_file(file_id)
-            if file:
-                filename = save_to_tmp(file, 'voice_', end='.ogg')
-                # распознаем
-                text = ya_app.recognize(filename)
-                # if os.path.exists(filename):
-                os.remove(filename)
-                # bot.send_message(message.chat_id, "Recognize: \n" + text)
-                # перевод
-                ya_app.translate(text, settings=user)
-                if self.config.tts_enable and user.tts:
-                    voice = ya_app.tts(ya_app.text, settings=user).file
-                    if os.path.exists(voice):
-                        with open(voice, 'rb') as f:
-                            data_file = f.read()
-                        bot.send_voice(message.chat_id, voice=data_file, caption=text + "\n" + ya_app.text)
-                        os.remove(voice)
-                # текстовое сообщение
-                else:
-                    bot.send_message(message.chat_id, ya_app.text)
+                    # Обрабатываем голосовое сообщение
+                    if message.voice:
+                        file_id = message.voice.get('file_id')
+                        file = bot.get_file(file_id)
+                        if file:
+                            filename = save_to_tmp(file, 'voice_', end='.ogg')
+                            # распознаем
+                            if filename:
+                                text = ya_app.recognize(filename)
+                                # if os.path.exists(filename):
+                                os.remove(filename)
+                                # перевод
+                                ya_app.translate(text, settings=user)
+                                if self.config.tts_enable and user.tts:
+                                    voice = ya_app.tts(ya_app.text, settings=user).file
+                                    # if os.path.exists(voice):
+                                    with open(voice, 'rb') as f:
+                                        data_file = f.read()
+                                    bot.send_voice(message.chat_id, voice=data_file, caption=text + "\n" + ya_app.text)
+                                    user.attempt_decrement()
+                                    os.remove(voice)
+                                # текстовое сообщение
+                                else:
+                                    bot.send_message(message.chat_id, ya_app.text)
+                                # Уменьшаем остаток проб перевода
+                                user.attempt_decrement()
 
-            else:
-                bot.send_message(message.chat_id, 'Ошибка загрузки ауди сообщения')
+                        else:
+                            bot.send_message(message.chat_id, 'Ошибка загрузки ауди сообщения')
 
-    def run(self):
+        except Exception as err:
+            logger.error('Voicer.handler error ' + str(err))
+
+    async def run(self):
+        with Data() as d:
+            update_id = d.get('update_id') or 1
+
+        # save_id = update_id
+        while True:
+            result: TelegramResult = bot.get_update(update_id + 1)
+
+            # Обрабатываем все сообщения
+            handlers = [self.handler(message) for message in result.messages]
+            await asyncio.gather(*handlers)
+
+            # Сохраняем номер последнего сообщения
+            last_message: list[TelegramMessage] = result.messages[-1:]
+            if len(last_message) and update_id < last_message[0].update_id:
+                update_id = last_message[0].update_id
+                with Data() as d:
+                    d.set('update_id', last_message[0].update_id)
+
+            await asyncio.sleep(1)
+
+    def start(self):
         try:
+            print("Запускаем программу")
             logger.debug("Start app")
-            with Data() as d:
-                update_id = d.get('update_id') or 1
-
-            # save_id = update_id
-            while True:
-                result: TelegramResult = bot.get_update(update_id + 1)
-
-                for message in result.messages:
-                    # save_id = message.update_id
-                    self.handler(message)
-
-                # Сохраняем номер последнего сообщения
-                last_message: list[TelegramMessage] = result.messages[-1:]
-                if len(last_message) and update_id < last_message[0].update_id:
-                    update_id = last_message[0].update_id
-                    with Data() as d:
-                        d.set('update_id', last_message[0].update_id)
-
-                sleep(1)
+            asyncio.run(self.run())
         except KeyboardInterrupt:
             print("Завершаем программу")
             logger.debug("Stop app")
--- core/classes/user/__init__.py
+++ core/classes/user/__init__.py
@@ -1,10 +1,14 @@
 import os
 import json
+from datetime import datetime
 
 from core.classes.data import Data
 from core.classes.telegram import TelegramMessage
 from core.core import Command
 from core.utils import command_split
+from core.logger import logger
+
+DEFAULT_ATTEMPTS_PER_DAY = 10
 
 
 class UserDataNotHaveUserId(Exception):
@@ -29,6 +33,8 @@ class User:
     target_lang: str = 'en'
     emotion: str = 'goo'
     tts: int = 1
+    attempts_per_day: int = DEFAULT_ATTEMPTS_PER_DAY
+    last_attempt: datetime = datetime.now()
 
     data = {}
 
@@ -42,17 +48,31 @@ class User:
             self.target_lang = d.get('lang') or 'en'
             self.tts = int(d.get('tts')) if d.get('tts') is not None else 1
             self.emotion = d.get('emotion') or 'good'
+            self.attempts_per_day = int(d.get('attempts_per_day')) if d.get('attempts_per_day') is not None else 10
+            self.last_attempt = datetime.strptime(d.get('last_attempt'), '%d.%m.%Y') \
+                if d.get('last_attempt') is not None else DEFAULT_ATTEMPTS_PER_DAY
 
     def __call__(self, *args, **kwargs):
         return self.user_id
 
+    def attempt_decrement(self):
+        delta = datetime.now() - self.last_attempt
+        if delta.days > 0:
+            self.attempts_per_day = DEFAULT_ATTEMPTS_PER_DAY
+        self.attempts_per_day -= 1
+        with UserData(self.user_id) as d:
+            d.set('attempts_per_day', self.attempts_per_day)
+            d.set('last_attempt', datetime.now().strftime('%d.%m.%Y'))
+
     def command_processing(self, message: TelegramMessage):
         with UserData(self.user_id) as d:
-            command, value = command_split(message.text)
+            command, value = command_split(message.text, '_')
             cmd = Command()
             try:
                 if cmd.list().index(command) >= 0:
                     cmd.get(command)
                     d.set(command, cmd.type(value) if value else cmd.default)
-            except ValueError:
-                pass
+                    logger.debug(f'User.command_processing\t\ncommand "{command}", value "{value}"')
+                    return command, value
+            except ValueError as err:
+                logger.debug(f'User.command_processing Error {str(err)}\t\ntext "{message.text}"')
